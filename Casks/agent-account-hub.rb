436b26083ce093407318869e552e5e97ed29b746
liumingyue <liumingyue@powerlaw.ai>
2026-03-30 18:06:53 +0800

    Update agent-account-hub to v0.1.5

--- Casks/agent-account-hub.rb
+++ Casks/agent-account-hub.rb
@@ -1,6 +1,6 @@
 cask "agent-account-hub" do
-  version "0.1.4"
-  sha256 "486525637b9180e34cfe88175a9c02542ddd0e0f578f33b396405d224adc712c"
+  version "0.1.5"
+  sha256 "7bdbdfbbcb5f27cd7276222953a0804e28792efc37e41fd2a481f1771857fb3a"
 
   url "https://github.com/gitliu-my/agent-account-hub/releases/download/v#{version}/Agent.Account.Hub.zip",
       verified: "github.com/gitliu-my/agent-account-hub/"

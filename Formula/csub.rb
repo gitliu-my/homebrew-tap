fb4de17fa40b7cff2c7b001ba1e4da1b53789a30
小牛 <80045852+gitliu-my@users.noreply.github.com>
2026-08-16 11:14:17 +0800

    Update csub to v0.9.1

--- Formula/csub.rb
+++ Formula/csub.rb
@@ -1,9 +1,9 @@
 class Csub < Formula
   desc "Use Codex subscription models through a local OpenAI-compatible API"
   homepage "https://github.com/gitliu-my/codex_subscription_client"
-  url "https://github.com/gitliu-my/codex_subscription_client/releases/download/v0.9.0/csub-macos-arm64.tar.gz"
-  version "0.9.0"
-  sha256 "6feeb1ddac94248f79bfef8fd8eec5c56a813c81ec49c19dfb95f684fea1a115"
+  url "https://github.com/gitliu-my/codex_subscription_client/releases/download/v0.9.1/csub-macos-arm64.tar.gz"
+  version "0.9.1"
+  sha256 "9edd691b8d58629693356327b72681c2f54505b696b55e173eab19eab0146f1d"
 
   depends_on arch: :arm64
 
